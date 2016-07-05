4c620c62477650c65f44694e9a4d266ddf13c979
Daniel Paulino <danielpaulino030@gmail.com>
2016-07-05 11:51:18 -0300

    UMD

--- jquery.dragtable.js
+++ jquery.dragtable.js
@@ -51,8 +51,20 @@
 /*
  * Special thx to all pull requests comitters
  */
-
-(function($) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jQuery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('jQuery'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.jQuery);
+    }
+}(this, function ($) {
   $.widget("akottr.dragtable", {
     options: {
       revert: false,               // smooth revert
@@ -400,4 +412,4 @@
     b.parentNode.insertBefore(a, b);
     aparent.insertBefore(b, asibling);
   }
-})(jQuery);
+}));
